ebd8fa3b08ea5ceb18443f1577e24de40492bfec
Saiz <daniel.saiz@gmx.at>
2021-04-28 00:07:15 +0200

    implemented state step

--- covidVaccination-angular-frontend/src/app/components/patient-area/state-step/state-step.component.ts
+++ covidVaccination-angular-frontend/src/app/components/patient-area/state-step/state-step.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { State } from 'src/app/model/state';
+import { DataStorageService } from 'src/app/service/data-storage.service';
 import { StepperService } from 'src/app/service/stepper.service';
 
 @Component({
@@ -8,13 +10,21 @@ import { StepperService } from 'src/app/service/stepper.service';
 })
 export class StateStepComponent implements OnInit {
 
-  constructor(private stepperService: StepperService) { }
+  states: State[] = [];
+
+  constructor(private stepperService: StepperService,
+    private dataService: DataStorageService) { }
 
   ngOnInit(): void {
     console.log("State Step!");
+    this.dataService.getAllStates().subscribe((states) => {
+      this.states = states;
+      console.log("=== states: ", this.states);
+    })
   }
 
-  next() {
+  next(state: State) {
+    this.stepperService.steps[1].data = state;
     this.stepperService.currentStepIndex.next(2);
   }
 
