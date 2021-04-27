6beb34ddd1461405bc0deb1d8888195e1f8770d7
Saiz <daniel.saiz@gmx.at>
2021-04-28 00:06:19 +0200

    added api calls to dataservice

--- covidVaccination-angular-frontend/src/app/service/data-storage.service.ts
+++ covidVaccination-angular-frontend/src/app/service/data-storage.service.ts
@@ -1,7 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+import { State } from '../model/state';
+import { Location } from '../model/location';
+import { Vaccination } from '../model/vaccination';
 
 @Injectable()
 export class DataStorageService {
 
-  constructor() { }
+  private BASE_URL: string = "http://covidvaccination.s1810456031.student.kwmhgb.at/api";
+
+  constructor(private http: HttpClient) { }
+
+  getAllStates(): Observable<Array<State>> {
+    return this.http.get(`${this.BASE_URL}/states`).pipe(retry(3)).pipe(catchError(this.errorHandler));
+  }
+
+  getLocationsOfState(state: string): Observable<Array<Location>> {
+    return this.http.get(`${this.BASE_URL}/state/${state}/locations`).pipe(retry(3)).pipe(catchError(this.errorHandler));
+  }
+
+  getVaccinationsOfState(state: string): Observable<Array<Vaccination>> {
+    return this.http.get(`${this.BASE_URL}/vaccinations/state/${state}`).pipe(retry(3)).pipe(catchError(this.errorHandler));
+  }
+
+
+  private errorHandler(error: Error | any): Observable<any> {
+    return throwError(error);
+  }
 }
