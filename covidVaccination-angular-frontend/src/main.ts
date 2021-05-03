2ce440f15f426028fe22c882a6035bd07b529f58
Saiz <daniel.saiz@gmx.at>
2021-05-04 00:21:30 +0200

    turn off console logs for production

--- covidVaccination-angular-frontend/src/main.ts
+++ covidVaccination-angular-frontend/src/main.ts
@@ -4,8 +4,14 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-if (environment.production) {
+ if (environment.production) {
   enableProdMode();
+  if(window){
+    window.console.log=function(){};
+    window.console.warn=function(){};
+    window.console.error=function(){};
+    window.console.table=function(){};
+  }
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
